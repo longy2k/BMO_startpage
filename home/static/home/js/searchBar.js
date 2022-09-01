69ecfed77f7e0a129753d61ed3ed70c2daf3a19b
Long Huynh <longhuynh.y2k@protonmail.com>
2022-09-01 13:36:17 -0400

    Added Search capabilities

--- home/static/home/js/searchBar.js
+++ home/static/home/js/searchBar.js
@@ -5,21 +5,46 @@ searchBar.addEventListener("keyup", function(event) {
     var searchValue = searchBar.value;
     var splitValue = searchValue.split(" ");
 
-    if(splitValue[0].charAt(0) == "/") {
-      alert("YEAH");
+    // COMMANDS
+    if (splitValue[0].charAt(0) == "/") {
+      var command = splitValue.shift();
+      var searchText = splitValue.join(" ");
+      switch (command.substring(1)) {
+        case 'a': //Amazon
+          window.open("https://www.amazon.com/s?k=" + searchText, "_self") // Working on this part ATM!
+          break;
+        case 'yt': //YouTube
+          window.open("https://www.youtube.com/results?search_query=" + searchText, "_self");
+          break;
+        case 'g': //Google
+          window.open("https://www.google.com/search?q=" + searchText, "_self");
+          break;
+        case 'github': //Github Search
+          window.open("https://github.com/search?q=" + searchText, "_self");
+          break;
+        case 'ddg': //DuckDuckGo
+          window.open("https://duckduckgo.com/?q=" + searchText, "_self");
+          break;
+        case 'steam': //Steam
+          window.open("https://store.steampowered.com/search/?term=" + searchText, "_self");
+          break;
+        case 'sof': //StackOverFlow
+          window.open("https://stackoverflow.com/search?q=" + searchText, "_self");
+          break;
+        case 'reddit':
+          window.open("https://old.reddit.com/search/?q=" + searchText, "_self");
+          break;
+        case 'subreddit':
+          window.open("https://old.reddit.com/r/" + searchText, "_self");
+          break;
+        case 'umb': //University of Massachusetts Boston Search
+          window.open("https://www.umb.edu/search?q=" + searchText, "_self");
+          break;
+
+      }
     }
     else {
       window.open("https://www.google.com/search?q=" + searchValue, "_self")
     }
-
-    console.log(searchValue.substring(1));
-
-    // switch (splitValue[0].substring(1)) {
-    //   case 'a':
-    //     !searchValue.includes(" ") ? window.open("https://www.amazon.com/", "_blank") : window.open("https://www.amazon.com/s?k=" + splitValue[1], "_blank");
-    //     break;
-    //   default:
-    //     window.open("https://www.google.com/search?q=" + splitValue[0], "_self");
-    // }
   }
 });
